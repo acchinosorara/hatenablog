a863141e853e8b734da03b8e4928b8c75a336d5f
gitTest <dq.ta.a1370@gmail.com>
2024-10-30 11:54:38 +0900

    'body取得の記述を修正'

--- hatenablog.js
+++ hatenablog.js
@@ -1,8 +1,7 @@
-const body = getElementByTagName('body')
 const pageName = ['oberonia']
 let isClass
 for (let i = 0; i < pageName.length; i++) {
-    isClass = body.classList.contains(`static-page-${pageName[i]}`)
+    isClass = document.body.classList.contains(`static-page-${pageName[i]}`)
 }
 
 if (!isClass) {
