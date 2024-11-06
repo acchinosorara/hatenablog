6bc912a769a5412816b7f8cd760f1f958a50c49e
gitTest <dq.ta.a1370@gmail.com>
2024-11-07 00:37:39 +0900

    ハンバーガーメニュー開閉時の処理を追加

--- hatenablog.js
+++ hatenablog.js
@@ -1,11 +1,29 @@
-const pageName = ['oberonia']
-let isClass
-for (let i = 0; i < pageName.length; i++) {
-    isClass = document.body.classList.contains(`static-page-${pageName[i]}`)
-}
+// ハンバーガーメニュー切替時のclass付与
+const toggleClassElement = document.getElementById('toggle_wrapper').children
+toggleClassElement[2].addEventListener('click', () => {
+    for (let i = 0; i < toggleClassElement.length; i++) {
+        if (i !== 0) {
+            toggleClassElement[i].classList.toggle('active')
+        }
+    }
+})
 
-if (!isClass) {
-    console.log('このページではありません')
-} else {
-    console.log('このページです')
+const entryHeaderMove = () => {
+    
+    // キャラクターページか判定する
+    const pageName = ['oberonia']
+    let isClass
+    for (let i = 0; i < pageName.length; i++) {
+        isClass = document.body.classList.contains(`static-page-${pageName[i]}`)
+    }
+
+    // キャラクターページの場合、キャラ名を移動
+    if (!isClass) {
+        return
+    } else {
+        const entryHeader = document.querySelector('.entry-header')
+        const characterProfileName = document.querySelector('.character-profile-name')
+        characterProfileName.appendChild(entryHeader)
+    }
 }
+entryHeaderMove()
